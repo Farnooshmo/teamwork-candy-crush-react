dd44714ecb103a0526e05c82cc608684cb772bae
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-04-30 12:44:00 +0100

    Dragging done

--- src/components/checkForColumnOfThree.js
+++ src/components/checkForColumnOfThree.js
@@ -1,13 +1,20 @@
-
+import blank from "../images/blank-candy.png";
 const checkForColumnOfThree = (width, currentColorArrangement) => {
-    for  (let i =  0; i <= 47 ; i++) {
-        const columnOfThree = [i, i +  width, i +  width * 2]
-        const decidedColor = currentColorArrangement[i]
-  
-        if (columnOfThree.every(square => currentColorArrangement[square] === decidedColor)) {
-              columnOfThree.forEach(square => currentColorArrangement[square] = '')
-        }
-      }
-}
+  for (let i = 0; i <= 47; i++) {
+    const columnOfThree = [i, i + width, i + width * 2];
+    const decidedColor = currentColorArrangement[i];
 
-export default checkForColumnOfThree
+    if (
+      columnOfThree.every(
+        (square) => currentColorArrangement[square] === decidedColor
+      )
+    ) {
+      columnOfThree.forEach(
+        (square) => (currentColorArrangement[square] = blank)
+      );
+      return true;
+    }
+  }
+};
+
+export default checkForColumnOfThree;
--- src/components/checkForRowOfFour.js
+++ src/components/checkForRowOfFour.js
@@ -1,17 +1,28 @@
-
-
+import blank from "../images/blank-candy.png";
 const checkForRowOfFour = (width, currentColorArrangement) => {
-    for  (let i =  0; i <= 7 ; i++) {
-        for (let j = 0; j <= 3; j++) {
-          let firstIndex = j + (width * i)
-          const rowOfFour = [firstIndex, firstIndex + 1, firstIndex + 2, firstIndex +  3]
-          const decidedColor = currentColorArrangement[firstIndex]
-  
-        if (rowOfFour.every(square => currentColorArrangement[square] === decidedColor)) {
-              rowOfFour.forEach(square => currentColorArrangement[square] = '')
-        }
-        }
-      }
-}
+  for (let i = 0; i <= 7; i++) {
+    for (let j = 0; j <= 3; j++) {
+      let firstIndex = j + width * i;
+      const rowOfFour = [
+        firstIndex,
+        firstIndex + 1,
+        firstIndex + 2,
+        firstIndex + 3,
+      ];
+      const decidedColor = currentColorArrangement[firstIndex];
 
-export default checkForRowOfFour
+      if (
+        rowOfFour.every(
+          (square) => currentColorArrangement[square] === decidedColor
+        )
+      ) {
+        rowOfFour.forEach(
+          (square) => (currentColorArrangement[square] = blank)
+        );
+        return true;
+      }
+    }
+  }
+};
+
+export default checkForRowOfFour;
